2690839c55ab4bdced0367279b02156ab2934526
Алексей Веревкин <alekseverevkin@gmail.com>
2024-10-21 13:41:49 +0500

    working on 21.10 13:41

--- 2nd year/QT 1/Homework/3.py
+++ 2nd year/QT 1/Homework/3.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget, QApplication, QPushButton, QRadioButton
+from PyQt6.QtWidgets import QWidget, QApplication, QPushButton, QRadioButton, QLabel
 import sys
 
 
@@ -8,25 +8,53 @@ class TicTacToe(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 500, 500)
+        self.setGeometry(300, 300, 190, 300)
         self.x_radio = QRadioButton("X", self)
-        self.x_radio.move(200, 20)
+        self.x_radio.move(60, 20)
         self.x_radio.setChecked(True)
-        self.y_radio = QRadioButton("Y", self)
-        self.y_radio.move(250, 20)
+        self.x_radio.clicked.connect(self.change)
+        self.o_radio = QRadioButton("O", self)
+        self.o_radio.move(110, 20)
+        self.o_radio.clicked.connect(self.change)
         self.button_grid = [
             QPushButton('', self), QPushButton('', self), QPushButton('', self),
             QPushButton('', self), QPushButton('', self), QPushButton('', self),
             QPushButton('', self), QPushButton('', self), QPushButton('', self),
         ]
+        [item.resize(50, 50) for item in self.button_grid]
+        [item.clicked.connect(self.fill) for item in self.button_grid]
         self.def_x = 10
         self.def_y = 50
         for i in range(3):
-            for j in range(len(self.button_grid)):
-                self.button_grid[i].move(self.def_x, self.def_y + 60 * j)
-                self.button_grid[i].resize(50, 50)
-                self.def_x += 60
-            self.def_x = 10
+            self.button_grid[(i * 3)].move(10, self.def_y)
+            self.button_grid[(i * 3) + 1].move(70, self.def_y)
+            self.button_grid[(i * 3) + 2].move(130, self.def_y)
+            self.def_y += 60
+        self.result = QLabel('', self)
+        self.result.move(85, 240)
+        self.new_game_button = QPushButton('Новая игра', self)
+        self.new_game_button.move(60, 260)
+        self.new_game_button.clicked.connect(self.reset)
+        self.text = 'X'
+        
+    def change(self):
+        if self.sender().text() == 'X':
+            self.text = 'X'
+        else:
+            self.text = 'O'
+        self.reset()
+
+    def reset(self):
+        [item.setText('') for item in self.button_grid]
+        self.result.setText('')
+
+    def fill(self):
+        if self.sender().text() == '':
+            self.sender().setText(self.text)
+            if self.text == "X":
+                self.text = "O"
+            else:
+                self.text = "X"
 
 
 
