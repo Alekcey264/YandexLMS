95e0d5cd749bc97534232333e21d3bdf22d7e717
Alekcey264 <alekseverevkin@gmail.com>
2025-01-14 10:42:34 +0500

    working on 14.01 10:42

--- 2nd year/WEB/7. Flask introduction/Classroom work/4.py
+++ 2nd year/WEB/7. Flask introduction/Classroom work/4.py
@@ -67,33 +67,5 @@ def promotion_image():
             </html>"""
 
 
-@app.route("/form_for_candidates", methods=["POST", "GET"])
-def form_for_candidates():
-    if request.method == "GET":
-        return f'''<!doctype html>
-                        <html lang="en">
-                            <head>
-                                <meta charset="utf-8">
-                                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                                <link rel="stylesheet"
-                                href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                                integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                                crossorigin="anonymous">
-                                <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                                <title>Пример формы</title>
-                            </head>
-                            <body>
-                                <h1>Анкета претендента</h1>
-                                <h2>на участие в миссии</h2>
-                                <div>
-                                hey 
-                                    <form class="login_form" method="post>
-                                        <input type="password" class="form-control" id="password" placeholder="Введите пароль" name="password">
-                                    </form>
-                                </div>
-                            </body>
-                        </html>'''
-
-
 if __name__ == "__main__":
     app.run(port=8080, host="127.0.0.1")
