cf96e58eba710b242d9ade2a13d2b6a702725972
Alekcey264 <alekseverevkin@gmail.com>
2025-02-03 02:22:41 +0500

    working on 03.02 02:22

--- 2nd year/WEB/9. Flask-sqlalchemy introduction/Homework/3.py
+++ 2nd year/WEB/9. Flask-sqlalchemy introduction/Homework/3.py
@@ -1,10 +1,4 @@
-class _FakeJobs(Jobs):
-    def __repr__(self):
-        return '<Job>' + self.job
-    
-
-Jobs = _FakeJobs
 global_init(input().strip())
 db_sess = create_session()
 users = db_sess.query(Jobs).filter(Jobs.work_size < 20, not Jobs.is_finished).all()
-[print(item) for item in users]
+[print(f'<Job> {item.job}') for item in users]
