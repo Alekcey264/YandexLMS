2b01f0d0e3062217d201594172b52b6d23105eaf
Alekcey264 <alekseverevkin@gmail.com>
2025-02-14 10:54:57 +0500

    working on 14.02 10:54

---
+++ 2nd year/Tests/Point of testing/Additional tasks/1.py
@@ -0,0 +1,23 @@
+from yandex_testing_lesson import is_palindrome
+
+
+def is_palindrome_test():
+    data = (
+        # ('', False),
+        ('aba', True),
+        ('a', True),
+        ('abc', False)
+    )
+    flag = True
+    for input_val, correct_output_val in data:
+        try:
+            output_val = is_palindrome(input_val)
+        except Exception:
+            flag = False
+        else:
+            if output_val != correct_output_val:
+                flag = False
+    if flag:
+        print('YES')
+    else:
+        print('NO')
