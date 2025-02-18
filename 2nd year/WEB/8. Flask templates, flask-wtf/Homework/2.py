e3bfa9a574cbb7515b7b65ab0518809b8138f34e
Alekcey264 <alekseverevkin@gmail.com>
2025-02-18 09:23:08 +0500

    working on 18.12 09:23

--- 2nd year/WEB/8. Flask templates, flask-wtf/Homework/2.py
+++ 2nd year/WEB/8. Flask templates, flask-wtf/Homework/2.py
@@ -1,19 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
-@app.route('/distribution')
-def distribution():
-    passengers = [
-        'Ridley',
-        'Andy',
-        'Mark',
-        'Venkata',
-        'Teddy',
-        'Shaun'
-    ]
-    return render_template('1.html', title='Пассажиры', passengers=passengers)
+@app.route('/table/<sex>/<age>')
+def table(sex, age):
+    return render_template('2.html', sex=sex, age=int(age))
 
 
 if __name__ == "__main__":
--- 2nd year/WEB/9. Flask-sqlalchemy introduction/Homework/4.py
+++ 2nd year/WEB/9. Flask-sqlalchemy introduction/Homework/4.py
@@ -2,10 +2,13 @@ global_init(input().strip())
 db_sess = create_session()
 jobs = db_sess.query(Jobs).all()
 jobs = [(item.team_leader, len(item.collaborators.split(', '))) for item in jobs]
-jobs.sort(key=lambda x: x[1])
+jobs.sort(key=lambda x: -x[1])
 max_len = jobs[0][1]
-jobs = list(filter(lambda x: x[1] == max_len, jobs))
+jobs = [item[0] for item in jobs if item[1] == max_len]
+unique = []
 for item in jobs:
-    leader = db_sess.query(User).filter(User.id == item[0]).first()
-    print(leader.name, leader.surname)
-
+    if item not in unique:
+        unique.append(item)
+for item in unique:
+    lead = str(db_sess.query(User).filter(User.id == item).all()[0]).split()
+    print(lead[-1], lead[-2], sep=' ')
