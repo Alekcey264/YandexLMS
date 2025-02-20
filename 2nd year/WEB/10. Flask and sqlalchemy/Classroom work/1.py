b07cb9409fa2a263775c5b065ffb8e5f7b6294a7
Alekcey264 <alekseverevkin@gmail.com>
2025-02-20 18:51:36 +0500

    working on 20.02 18:51

---
+++ 2nd year/WEB/10. Flask and sqlalchemy/Classroom work/1.py
@@ -0,0 +1,13 @@
+from flask_login import LoginManager
+from flask import Flask
+
+
+app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
